0b6836b075645fbc66c84b362471618eb3dc292c
Sammy Barasa <barsamms@gmail.com>
2019-10-23 10:40:04 +0300

    Updated splash activity to better handle crashes Added save icon resource
    Signed-off-by: Sammy Barasa <barsamms@gmail.com>

--- app/src/main/java/com/iita/akilimo/views/activities/SplashActivity.kt
+++ app/src/main/java/com/iita/akilimo/views/activities/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.iita.akilimo.views.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.crashlytics.android.Crashlytics
 import com.iita.akilimo.inherit.BaseActivity
 import com.iita.akilimo.utils.FireBaseConfig
 
@@ -9,29 +11,34 @@ class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initComponent()
+        try {
+            launchActivity()
+        } catch (ex: Exception) {
+            Crashlytics.log(Log.ERROR, LOG_TAG, "Unable to run thread for splash screen")
+            Crashlytics.logException(ex)
+            launchActivity() //fallback launching
+        }
+    }
+
+    override fun initComponent() {
+        try {
+            val fireBaseConfig = FireBaseConfig(this)
+            fireBaseConfig.fetchNewRemoteConfig()
+
+            val background = object : Thread() {
+                override fun run() {
+                    launchActivity()
+                }
+            }
+            background.start()
+        } catch (ex: Exception) {
+            Crashlytics.log(Log.ERROR, LOG_TAG, "Error running background thread for splash screen")
+            Crashlytics.logException(ex)
+        }
     }
 
     override fun initToolbar() {
         throw UnsupportedOperationException()
-    }
-
-    override fun initComponent() {
-        val fireBaseConfig = FireBaseConfig(this)
-        fireBaseConfig.fetchNewRemoteConfig()
-
-        val background = object : Thread() {
-            override fun run() {
-                try {
-                    val intent = Intent(this@SplashActivity, HomeActivity::class.java)
-                    startActivity(intent)
-                    closeActivity(false)
-                } catch (ex: Exception) {
-                }
-
-            }
-        }
-        background.start()
     }
 
     override fun validate(backPressed: Boolean) {
@@ -40,5 +47,11 @@ class SplashActivity : BaseActivity() {
 
     override fun closeActivity(backPressed: Boolean) {
         finish()
+    }
+
+    private fun launchActivity() {
+        val intent = Intent(this@SplashActivity, FertilizersActivity::class.java)
+        startActivity(intent)
+        closeActivity(false)
     }
 }
