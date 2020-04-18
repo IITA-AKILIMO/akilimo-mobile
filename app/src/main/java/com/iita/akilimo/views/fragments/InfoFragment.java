0ee6a0b8b61c581422c5895957a2e75e68178e01
Sammy Barasa <barsamms@gmail.com>
2020-04-18 14:41:39 +0300

    Bumped dev version number

---
+++ app/src/main/java/com/iita/akilimo/views/fragments/InfoFragment.java
@@ -0,0 +1,29 @@
+package com.iita.akilimo.views.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.iita.akilimo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InfoFragment extends Fragment {
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+}
