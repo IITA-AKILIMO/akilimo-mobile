de296cb0e16f872a3525092c2d35a24e0800f66c
Sammy Barasa <barsamms@gmail.com>
2020-05-04 03:20:31 +0300

    Added kenya swahili
    Signed-off-by: Sammy Barasa <barsamms@gmail.com>

--- app/src/main/java/com/iita/akilimo/Locales.java
+++ app/src/main/java/com/iita/akilimo/Locales.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 public class Locales {
     private static final Locale LOCAL_TZ_SWA = new Locale("sw", "TZ");
+    private static final Locale LOCAL_KE_SWA = new Locale("sw", "KE");
 
     public static final List<Locale> APP_LOCALES =
             Arrays.asList(Locale.ENGLISH, Locales.LOCAL_TZ_SWA);
