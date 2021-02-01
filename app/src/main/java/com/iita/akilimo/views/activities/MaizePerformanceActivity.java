6c3bade37082f3fb16c415c78f105ef93eb2a2d5
Sammy Barasa <barsamms@gmail.com>
2021-02-01 19:56:37 +0300

    Updated maize performance display

--- app/src/main/java/com/iita/akilimo/views/activities/MaizePerformanceActivity.java
+++ app/src/main/java/com/iita/akilimo/views/activities/MaizePerformanceActivity.java
@@ -3,22 +3,31 @@ package com.iita.akilimo.views.activities;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.common.util.Strings;
 import com.iita.akilimo.R;
+import com.iita.akilimo.adapters.MaizePerformanceAdapter;
 import com.iita.akilimo.dao.AppDatabase;
 import com.iita.akilimo.databinding.ActivityMaizePerformanceActivityBinding;
 import com.iita.akilimo.entities.MaizePerformance;
 import com.iita.akilimo.inherit.BaseActivity;
+import com.iita.akilimo.utils.ItemAnimation;
+import com.iita.akilimo.utils.Tools;
+import com.iita.akilimo.views.fragments.dialog.MaizePerformanceDialogFragment;
+import com.iita.akilimo.views.fragments.dialog.RootYieldDialogFragment;
+import com.iita.akilimo.widget.SpacingItemDecoration;
 
-;
+;import java.util.ArrayList;
+import java.util.List;
 
 
 public class MaizePerformanceActivity extends BaseActivity {
@@ -27,19 +36,31 @@ public class MaizePerformanceActivity extends BaseActivity {
     String richSoil;
 
     Toolbar toolbar;
-    RadioGroup rdgMaizePerformance;
+    RecyclerView recyclerView;
+    View viewPos;
+
     AppCompatButton btnFinish;
     AppCompatButton btnCancel;
     TextView exceptionTitle;
 
+    private MaizePerformanceAdapter mAdapter;
+
     ActivityMaizePerformanceActivityBinding binding;
 
 
-    private MaizePerformance maizePerformance;
+    private MaizePerformance savedMaizePerformance;
 
-    private String selectedMaizePerformance;
+    private String selectedPerformanceValue;
     private String maizePerformanceValue;
     private int performanceRadioIndex;
+
+    private final Integer[] performanceImages = {
+            R.drawable.yield_less_than_7point5,
+            R.drawable.yield_7point5_to_15,
+            R.drawable.yield_15_to_22point5,
+            R.drawable.yield_22_to_30,
+            R.drawable.yield_more_than_30,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +71,17 @@ public class MaizePerformanceActivity extends BaseActivity {
         database = AppDatabase.getDatabase(context);
 
         toolbar = binding.toolbar;
-        rdgMaizePerformance = binding.rdgMaizePerformance;
+        recyclerView = binding.rootYieldRecycler;
+        viewPos = binding.coordinatorLayout;
+
         exceptionTitle = binding.exceptionTitle;
         btnFinish = binding.twoButtons.btnFinish;
         btnCancel = binding.twoButtons.btnCancel;
 
+        savedMaizePerformance = database.maizePerformanceDao().findOne();
+        if (savedMaizePerformance != null) {
+            selectedPerformanceValue = savedMaizePerformance.getMaizePerformance();
+        }
         initToolbar();
         initComponent();
     }
@@ -82,71 +109,86 @@ public class MaizePerformanceActivity extends BaseActivity {
 
         btnFinish.setOnClickListener(view -> validate(false));
         btnCancel.setOnClickListener(view -> closeActivity(false));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        recyclerView.addItemDecoration(new SpacingItemDecoration(1, Tools.dpToPx(this, 3), true));
+        recyclerView.setHasFixedSize(true);
 
-        rdgMaizePerformance.setOnCheckedChangeListener((radioGroup, radioIndex) -> {
-            exceptionTitle.setVisibility(View.GONE);
-            switch (radioIndex) {
-                case R.id.rdMaizeKneeHigh:
-                    selectedMaizePerformance = "50";
-                    maizePerformanceValue = "1";
-                    exceptionTitle.setVisibility(View.VISIBLE);
-                    exceptionTitle.setText(poorSoil);
-                    break;
-                case R.id.rdMaizeChestHigh:
-                    selectedMaizePerformance = "150";
-                    maizePerformanceValue = "2";
-                    break;
-                case R.id.rdMaizeYellowLeaves:
-                    selectedMaizePerformance = "yellow";
-                    maizePerformanceValue = "3";
-                    break;
-                case R.id.rdMaizeGreenLeaves:
-                    selectedMaizePerformance = "green";
-                    maizePerformanceValue = "4";
-                    break;
-                case R.id.rdMaizeDarkGreenLeaves:
-                    selectedMaizePerformance = "dark green";
-                    maizePerformanceValue = "5";
-                    exceptionTitle.setVisibility(View.VISIBLE);
-                    exceptionTitle.setText(richSoil);
-                    break;
+        List<MaizePerformance> items = new ArrayList<>();
+
+        items.add(createPerformanceObject(performanceImages[0], poorSoil, "1", "50", getString(R.string.lbl_knee_height)));
+        items.add(createPerformanceObject(performanceImages[1], null, "2", "150", getString(R.string.lbl_chest_height)));
+        items.add(createPerformanceObject(performanceImages[2], null, "3", "yellow", getString(R.string.lbl_yellowish_leaves)));
+        items.add(createPerformanceObject(performanceImages[3], null, "4", "green", getString(R.string.lbl_green_leaves)));
+        items.add(createPerformanceObject(performanceImages[4], richSoil, "5", "dark green", getString(R.string.lbl_dark_green_leaves)));
+
+        mAdapter = new MaizePerformanceAdapter(this, items, ItemAnimation.FADE_IN);
+        recyclerView.setAdapter(mAdapter);
+        mAdapter.setItems(selectedPerformanceValue, items);
+
+
+        mAdapter.setOnItemClickListener((view, clickedMaizePerformance, position) -> {
+            try {
+                //show a popup dialog here
+                Bundle arguments = new Bundle();
+                arguments.putParcelable(MaizePerformanceDialogFragment.PERFORMANCE_DATA, clickedMaizePerformance);
+
+                MaizePerformanceDialogFragment rootYieldDialogFragment = new MaizePerformanceDialogFragment(context);
+                rootYieldDialogFragment.setArguments(arguments);
+
+                rootYieldDialogFragment.setOnDismissListener((yield, yieldConfirmed) -> {
+                    if (yieldConfirmed) {
+                        if (savedMaizePerformance == null) {
+                            savedMaizePerformance = new MaizePerformance();
+                        }
+                        String maizePerformance = yield.getMaizePerformance();
+                        selectedPerformanceValue = yield.getPerformanceValue();
+                        savedMaizePerformance.setMaizePerformance(maizePerformance);
+                        savedMaizePerformance.setPerformanceValue(selectedPerformanceValue);
+                        database.maizePerformanceDao().insert(savedMaizePerformance);
+
+                        mAdapter.setActiveRowIndex(position);
+                    }
+                    mAdapter.setItems(selectedPerformanceValue, items);
+                });
+
+
+                FragmentTransaction fragmentTransaction;
+                if (getFragmentManager() != null) {
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    Fragment prev = getSupportFragmentManager().findFragmentByTag(RootYieldDialogFragment.ARG_ITEM_ID);
+                    if (prev != null) {
+                        fragmentTransaction.remove(prev);
+                    }
+                    fragmentTransaction.addToBackStack(null);
+                    rootYieldDialogFragment.show(getSupportFragmentManager(), RootYieldDialogFragment.ARG_ITEM_ID);
+                }
+
+            } catch (Exception ex) {
+                Crashlytics.log(Log.ERROR, LOG_TAG, ex.getMessage());
+                Crashlytics.logException(ex);
             }
+
         });
+    }
 
-        //preset saved data if any
-        maizePerformance = database.maizePerformanceDao().findOne();
-        if (maizePerformance != null) {
-            performanceRadioIndex = maizePerformance.getPerformanceRadioIndex();
-            rdgMaizePerformance.check(performanceRadioIndex);
-        }
-
+    private MaizePerformance createPerformanceObject(Integer yieldImage, String performanceDesc, String performanceValue, String maizePerformance, String maizePerformanceLabel) {
+        MaizePerformance performance = new MaizePerformance();
+        performance.setImageId(yieldImage);
+        performance.setMaizePerformanceDesc(performanceDesc);
+        performance.setPerformanceValue(performanceValue);
+        performance.setMaizePerformance(maizePerformance);
+        performance.setMaizePerformanceLabel(maizePerformanceLabel);
+        return performance;
     }
 
     @Override
     protected void validate(boolean backPressed) {
 
-        if (Strings.isEmptyOrWhitespace(selectedMaizePerformance) || Strings.isEmptyOrWhitespace(maizePerformanceValue)) {
+        if (Strings.isEmptyOrWhitespace(selectedPerformanceValue) || Strings.isEmptyOrWhitespace(maizePerformanceValue)) {
             showCustomWarningDialog(getString(R.string.lbl_invalid_selection), getString(R.string.lbl_maize_performance_prompt));
             return;
         }
-
-        performanceRadioIndex = rdgMaizePerformance.getCheckedRadioButtonId();
-        try {
-
-            if (maizePerformance == null) {
-                maizePerformance = new MaizePerformance();
-            }
-            maizePerformance.setPerformanceRadioIndex(performanceRadioIndex);
-            maizePerformance.setMaizePerformance(selectedMaizePerformance);
-            maizePerformance.setPerformanceValue(maizePerformanceValue);
-
-            database.maizePerformanceDao().insert(maizePerformance);
-            closeActivity(backPressed);
-        } catch (Exception ex) {
-            Toast.makeText(context, ex.getMessage(), Toast.LENGTH_SHORT).show();
-            Crashlytics.log(Log.ERROR, LOG_TAG, ex.getMessage());
-            Crashlytics.logException(ex);
-        }
+        closeActivity(backPressed);
     }
 
 }
