e593fb949615b502308cae173b739ff9d1008e77
Masgeek <barsamms@gmail.com>
2025-04-22 12:57:49 +0300

    feat: update fertilizer table name and column names in FertilizerDao
    The commit updates the table name from "fertilizer" to "fertilizers" in the FertilizerDao class. It also updates the column names to use snake_case for consistency.

--- app/src/main/java/com/akilimo/mobile/dao/FertilizerDao.kt
+++ app/src/main/java/com/akilimo/mobile/dao/FertilizerDao.kt
@@ -1,28 +1,33 @@
 package com.akilimo.mobile.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.akilimo.mobile.entities.Fertilizer
 
 @Dao
 interface FertilizerDao {
 
-    @Query("SELECT * FROM fertilizer")
-    fun listAll(): List<Fertilizer>
+    @Query("SELECT * FROM fertilizers")
+    fun listAll(): MutableList<Fertilizer>
 
-    @Query("SELECT * FROM fertilizer LIMIT 1")
+    @Query("SELECT * FROM fertilizers LIMIT 1")
     fun findOne(): Fertilizer?
 
-    @Query("select * from fertilizer where fertilizerType=:fertilizerType")
+    @Query("select * FROM fertilizers where type=:fertilizerType")
     fun findByType(fertilizerType: String?): Fertilizer?
 
-    @Query("select * from fertilizer where fertilizerType=:fertilizerType and countryCode=:countryCode limit 1")
+    @Query("select * FROM fertilizers where type=:fertilizerType and country_code=:countryCode limit 1")
     fun findOneByTypeAndCountry(fertilizerType: String?, countryCode: String): Fertilizer?
 
-    @Query("SELECT * FROM fertilizer where countryCode=:countryCode and selected=1")
-    fun findAllSelectedByCountry(countryCode: String): List<Fertilizer>
+    @Query("SELECT * FROM fertilizers where country_code=:countryCode and selected=1")
+    fun findAllSelectedByCountry(countryCode: String): MutableList<Fertilizer>
 
-    @Query("SELECT * FROM fertilizer where countryCode=:countryCode")
-    fun findAllByCountry(countryCode: String): List<Fertilizer>
+    @Query("SELECT * FROM fertilizers where country_code=:countryCode")
+    fun findAllByCountry(countryCode: String): MutableList<Fertilizer>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(fertilizer: Fertilizer)
@@ -38,7 +43,8 @@ interface FertilizerDao {
     fun delete(fertilizer: Fertilizer?)
 
     @Delete
-    fun deleteFertilizerByList(fertilizerList: List<Fertilizer>)
-    @Query("DELETE FROM fertilizer")
+    fun deleteFertilizerByList(fertilizerList: MutableList<Fertilizer>)
+
+    @Query("DELETE FROM fertilizers")
     fun deleteAll()
 }
