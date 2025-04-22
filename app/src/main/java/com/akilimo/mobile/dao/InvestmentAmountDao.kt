a87b18cf4580cf3ca89a91a01cdb347ace6a7c10
Masgeek <barsamms@gmail.com>
2025-04-22 14:39:45 +0300

    feat: add methods to InvestmentAmountDao for finding by ID and batch insertion
    The changes introduce two new methods to the `InvestmentAmountDao` in Kotlin: `findOneByInvestmentId` to retrieve an investment amount based on its ID, and `insertAll` to insert a list of investment amounts into the database.

--- app/src/main/java/com/akilimo/mobile/dao/InvestmentAmountDao.kt
+++ app/src/main/java/com/akilimo/mobile/dao/InvestmentAmountDao.kt
@@ -1,19 +1,30 @@
 package com.akilimo.mobile.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.akilimo.mobile.entities.InvestmentAmount
 
 @Dao
 interface InvestmentAmountDao {
 
-    @Query("SELECT * FROM investment_amount")
+    @Query("SELECT * FROM investment_amounts")
     fun listAll(): List<InvestmentAmount>
 
-    @Query("SELECT * FROM investment_amount LIMIT 1")
+    @Query("SELECT * FROM investment_amounts LIMIT 1")
     fun findOne(): InvestmentAmount?
+
+    @Query("SELECT * FROM investment_amounts where id=:itemTagIndex LIMIT 1")
+    fun findOneByInvestmentId(itemTagIndex: Long): InvestmentAmount?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(vararg investmentAmount: InvestmentAmount)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(investmentList: List<InvestmentAmount>): LongArray?
 
     @Update
     fun update(vararg investmentAmount: InvestmentAmount?)
@@ -21,6 +32,6 @@ interface InvestmentAmountDao {
     @Delete
     fun delete(investmentAmount: InvestmentAmount?)
 
-    @Query("DELETE FROM investment_amount")
+    @Query("DELETE FROM investment_amounts")
     fun deleteAll()
 }
