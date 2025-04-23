05a44ce0ad7c42fb8fb7665c924c791ede37fa11
Masgeek <barsamms@gmail.com>
2025-04-23 13:13:27 +0300

    feat: migrate `WeedControlCostsActivity` from Java to Kotlin
    This commit migrates the `WeedControlCostsActivity` from Java to Kotlin. The migration includes:
    
    -   Replacing the Java class `WeedControlCostsActivity` with its Kotlin equivalent.
    -   Updating all related references to use the new Kotlin class.
    -   Ensuring all logic and functionality remains consistent with the previous Java implementation.
    -   Adapting to Kotlin syntax and features while preserving core functionality.
    -   Deleting the old Java `WeedControlCostsActivity`.

--- app/src/main/java/com/akilimo/mobile/views/activities/WeedControlCostsActivity.kt
+++ app/src/main/java/com/akilimo/mobile/views/activities/WeedControlCostsActivity.kt
@@ -1,243 +1,251 @@
-package com.akilimo.mobile.views.activities;
+package com.akilimo.mobile.views.activities
 
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.Toolbar
+import androidx.cardview.widget.CardView
+import com.akilimo.mobile.R
+import com.akilimo.mobile.dao.AppDatabase.Companion.getDatabase
+import com.akilimo.mobile.databinding.ActivityWeedControlCostBinding
+import com.akilimo.mobile.entities.AdviceStatus
+import com.akilimo.mobile.entities.CurrentPractice
+import com.akilimo.mobile.entities.FieldOperationCost
+import com.akilimo.mobile.inherit.BaseActivity
+import com.akilimo.mobile.utils.MathHelper
+import com.akilimo.mobile.utils.enums.EnumAdviceTasks
+import com.akilimo.mobile.utils.enums.EnumCountry
+import com.google.android.gms.common.util.Strings
+import io.sentry.Sentry
 
-import androidx.appcompat.widget.AppCompatButton;
-import androidx.appcompat.widget.AppCompatTextView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
+class WeedControlCostsActivity : BaseActivity() {
+    var toolbar: Toolbar? = null
+    var firstWeedingOpCostTitle: AppCompatTextView? = null
+    var secondWeedingOpCostTitle: AppCompatTextView? = null
+    var herbicideUseTitle: AppCompatTextView? = null
+    var herbicideUseCard: CardView? = null
+    var btnFinish: AppCompatButton? = null
+    var btnCancel: AppCompatButton? = null
+    var rdgWeedControl: RadioGroup? = null
+    var editFirstWeedingOpCost: EditText? = null
+    var editSecondWeedingOpCost: EditText? = null
 
-import com.akilimo.mobile.R;
-import com.akilimo.mobile.dao.AppDatabase;
-import com.akilimo.mobile.databinding.ActivityWeedControlCostBinding;
-import com.akilimo.mobile.entities.AdviceStatus;
-import com.akilimo.mobile.entities.AkilimoCurrency;
-import com.akilimo.mobile.entities.CurrentPractice;
-import com.akilimo.mobile.entities.FieldOperationCost;
-import com.akilimo.mobile.entities.MandatoryInfo;
-import com.akilimo.mobile.entities.ProfileInfo;
-import com.akilimo.mobile.inherit.BaseActivity;
-import com.akilimo.mobile.utils.MathHelper;
-import com.akilimo.mobile.utils.enums.EnumAdviceTasks;
-import com.akilimo.mobile.utils.enums.EnumCountry;
-import com.google.android.gms.common.util.Strings;
+    var binding: ActivityWeedControlCostBinding? = null
 
-import java.util.Locale;
+    private var mathHelper: MathHelper? = null
+    private var currentPractice: CurrentPractice? = null
+    private var fieldOperationCost: FieldOperationCost? = null
+    private var usesHerbicide = false
+    private var weedControlTechnique: String? = null
+    private var firstOperationCost = 0.0
+    private var secondOperationCost = 0.0
+    private var weedRadioIndex = 0
 
-import io.sentry.Sentry;
+    private val minCost = 1.0
+    private val maxCost = 0.0
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWeedControlCostBinding.inflate(
+            layoutInflater
+        )
+        setContentView(binding!!.root)
 
-public class WeedControlCostsActivity extends BaseActivity {
+        context = this
+        database = getDatabase(context)
+        mathHelper = MathHelper()
 
-
-    Toolbar toolbar;
-    AppCompatTextView firstWeedingOpCostTitle;
-    AppCompatTextView secondWeedingOpCostTitle;
-    AppCompatTextView herbicideUseTitle;
-    CardView herbicideUseCard;
-    AppCompatButton btnFinish;
-    AppCompatButton btnCancel;
-    RadioGroup rdgWeedControl;
-    EditText editFirstWeedingOpCost;
-    EditText editSecondWeedingOpCost;
-
-    ActivityWeedControlCostBinding binding;
-
-    private MathHelper mathHelper;
-    private CurrentPractice currentPractice;
-    private FieldOperationCost fieldOperationCost;
-    private boolean usesHerbicide;
-    private String weedControlTechnique;
-    private double firstOperationCost;
-    private double secondOperationCost;
-    private int weedRadioIndex;
-
-    private final double minCost = 1.0;
-    private double maxCost;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityWeedControlCostBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        context = this;
-        database = AppDatabase.getDatabase(context);
-        mathHelper = new MathHelper();
-
-        MandatoryInfo mandatoryInfo = database.mandatoryInfoDao().findOne();
+        val mandatoryInfo = database.mandatoryInfoDao().findOne()
         if (mandatoryInfo != null) {
-            areaUnit = mandatoryInfo.getAreaUnit();
-            fieldSize = mandatoryInfo.getAreaSize();
+            areaUnit = mandatoryInfo.areaUnit
+            fieldSize = mandatoryInfo.areaSize
         }
 
-        ProfileInfo profileInfo = database.profileInfoDao().findOne();
+        val profileInfo = database.profileInfoDao().findOne()
         if (profileInfo != null) {
-            countryCode = profileInfo.getCountryCode();
-            currency = profileInfo.getCurrency();
-            currencyCode = profileInfo.getCurrency();
-            AkilimoCurrency myAkilimoCurrency = database.currencyDao().findOneByCurrencyCode(currencyCode);
-            currencySymbol = myAkilimoCurrency.getCurrencySymbol();
-            currencyName = myAkilimoCurrency.getCurrencyName();
+            countryCode = profileInfo.countryCode
+            currency = profileInfo.currency
+            currencyCode = profileInfo.currency
+            val myAkilimoCurrency = database.currencyDao().findOneByCurrencyCode(currencyCode)
+            currencySymbol = myAkilimoCurrency.currencySymbol
+            currencyName = myAkilimoCurrency.currencyName
         }
 
-        fieldOperationCost = database.fieldOperationCostDao().findOne();
-        currentPractice = database.currentPracticeDao().findOne();
+        fieldOperationCost = database.fieldOperationCostDao().findOne()
+        currentPractice = database.currentPracticeDao().findOne()
 
-        toolbar = binding.toolbar;
-        firstWeedingOpCostTitle = binding.weedControlCosts.firstWeedingOpCostTitle;
-        secondWeedingOpCostTitle = binding.weedControlCosts.secondWeedingOpCostTitle;
-        herbicideUseTitle = binding.weedControlCosts.herbicideUseTitle;
-        herbicideUseCard = binding.weedControlCosts.herbicideUseCard;
-        btnFinish = binding.twoButtons.btnFinish;
-        btnCancel = binding.twoButtons.btnCancel;
-        rdgWeedControl = binding.weedControlCosts.rdgWeedControl;
-        editFirstWeedingOpCost = binding.weedControlCosts.editFirstWeedingOpCost;
-        editSecondWeedingOpCost = binding.weedControlCosts.editSecondWeedingOpCost;
+        toolbar = binding!!.toolbar
+        firstWeedingOpCostTitle = binding!!.weedControlCosts.firstWeedingOpCostTitle
+        secondWeedingOpCostTitle = binding!!.weedControlCosts.secondWeedingOpCostTitle
+        herbicideUseTitle = binding!!.weedControlCosts.herbicideUseTitle
+        herbicideUseCard = binding!!.weedControlCosts.herbicideUseCard
+        btnFinish = binding!!.twoButtons.btnFinish
+        btnCancel = binding!!.twoButtons.btnCancel
+        rdgWeedControl = binding!!.weedControlCosts.rdgWeedControl
+        editFirstWeedingOpCost = binding!!.weedControlCosts.editFirstWeedingOpCost
+        editSecondWeedingOpCost = binding!!.weedControlCosts.editSecondWeedingOpCost
 
 
-        initToolbar();
-        initComponent();
+        initToolbar()
+        initComponent()
     }
 
-    @Override
-    public void onBackPressed() {
-        validate(true);
+    override fun onBackPressed() {
+        validate(true)
     }
 
-    @Override
-    protected void initToolbar() {
-        toolbar.setNavigationIcon(R.drawable.ic_left_arrow);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(getString(R.string.title_weed_control));
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(v -> closeActivity(false));
+    override fun initToolbar() {
+        toolbar!!.setNavigationIcon(R.drawable.ic_left_arrow)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = getString(R.string.title_weed_control)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolbar!!.setNavigationOnClickListener { v: View? -> closeActivity(false) }
     }
 
-    @Override
-    protected void initComponent() {
-
-        Locale myLocale = getCurrentLocale();
-        String translatedUnit = context.getString(R.string.lbl_acre);
-        if (areaUnit.equals("ha")) {
-            translatedUnit = context.getString(R.string.lbl_ha);
+    override fun initComponent() {
+        val myLocale = currentLocale
+        var translatedUnit = context.getString(R.string.lbl_acre)
+        if (areaUnit == "ha") {
+            translatedUnit = context.getString(R.string.lbl_ha)
         }
-        String finalTranslatedUnit = translatedUnit.toLowerCase(myLocale);
-        switch (countryCode) {
-            case "TZ":
-                currencyName = EnumCountry.Tanzania.currencyName(context);
-                break;
-            case "NG":
-                currencyName = EnumCountry.Nigeria.currencyName(context);
-                break;
-            case "GH":
-                currencyName = EnumCountry.Ghana.currencyName(context);
-                break;
-            case "RW":
-                currencyName = EnumCountry.Rwanda.currencyName(context);
-                break;
-            case "BI":
-                currencyName = EnumCountry.Burundi.currencyName(context);
-                break;
+        val finalTranslatedUnit = translatedUnit.lowercase(myLocale)
+        when (countryCode) {
+            "TZ" -> currencyName = EnumCountry.Tanzania.currencyName(context)
+            "NG" -> currencyName = EnumCountry.Nigeria.currencyName(context)
+            "GH" -> currencyName = EnumCountry.Ghana.currencyName(context)
+            "RW" -> currencyName = EnumCountry.Rwanda.currencyName(context)
+            "BI" -> currencyName = EnumCountry.Burundi.currencyName(context)
         }
 
         if (currentPractice != null) {
-            weedRadioIndex = currentPractice.getWeedRadioIndex();
-            weedControlTechnique = currentPractice.getWeedControlTechnique();
-            rdgWeedControl.check(weedRadioIndex);
+            weedRadioIndex = currentPractice!!.weedRadioIndex
+            weedControlTechnique = currentPractice!!.weedControlTechnique
+            rdgWeedControl!!.check(weedRadioIndex)
         }
         if (fieldOperationCost != null) {
-            firstOperationCost = fieldOperationCost.getFirstWeedingOperationCost();
+            firstOperationCost = fieldOperationCost!!.firstWeedingOperationCost
             if (firstOperationCost > 0) {
-                editFirstWeedingOpCost.setText(String.valueOf(firstOperationCost));
+                editFirstWeedingOpCost!!.setText(firstOperationCost.toString())
             }
 
-            secondOperationCost = fieldOperationCost.getSecondWeedingOperationCost();
+            secondOperationCost = fieldOperationCost!!.secondWeedingOperationCost
             if (secondOperationCost > 0) {
-                editSecondWeedingOpCost.setText(String.valueOf(secondOperationCost));
+                editSecondWeedingOpCost!!.setText(secondOperationCost.toString())
             }
         }
-        String firstWeedCostTitle = getString(R.string.lbl_cost_of_first_weeding_operation, currencyName, mathHelper.removeLeadingZero(fieldSize), finalTranslatedUnit);
-        String secondWeedCostTitle = getString(R.string.lbl_cost_of_second_weeding_operation, currencyName, mathHelper.removeLeadingZero(fieldSize), finalTranslatedUnit);
-        if (myLocale.getLanguage().equals("sw")) {
-            firstWeedCostTitle = getString(R.string.lbl_cost_of_first_weeding_operation, currencyCode, finalTranslatedUnit, mathHelper.removeLeadingZero(fieldSize));
-            secondWeedCostTitle = getString(R.string.lbl_cost_of_second_weeding_operation, currencyCode, finalTranslatedUnit, mathHelper.removeLeadingZero(fieldSize));
+        var firstWeedCostTitle = getString(
+            R.string.lbl_cost_of_first_weeding_operation,
+            currencyName,
+            mathHelper!!.removeLeadingZero(fieldSize),
+            finalTranslatedUnit
+        )
+        var secondWeedCostTitle = getString(
+            R.string.lbl_cost_of_second_weeding_operation,
+            currencyName,
+            mathHelper!!.removeLeadingZero(fieldSize),
+            finalTranslatedUnit
+        )
+        if (myLocale.language == "sw") {
+            firstWeedCostTitle = getString(
+                R.string.lbl_cost_of_first_weeding_operation,
+                currencyCode,
+                finalTranslatedUnit,
+                mathHelper!!.removeLeadingZero(fieldSize)
+            )
+            secondWeedCostTitle = getString(
+                R.string.lbl_cost_of_second_weeding_operation,
+                currencyCode,
+                finalTranslatedUnit,
+                mathHelper!!.removeLeadingZero(fieldSize)
+            )
         }
-        firstWeedingOpCostTitle.setText(firstWeedCostTitle);
-        secondWeedingOpCostTitle.setText(secondWeedCostTitle);
+        firstWeedingOpCostTitle!!.text = firstWeedCostTitle
+        secondWeedingOpCostTitle!!.text = secondWeedCostTitle
 
-        rdgWeedControl.setOnCheckedChangeListener((radioGroup, radioIndex) -> {
-            switch (radioIndex) {
-                case R.id.rdManualOnlyControl:
-                    usesHerbicide = false;
-                    weedControlTechnique = "manual";
-                    break;
-                case R.id.rdHerbicideControl:
-                    usesHerbicide = true;
-                    weedControlTechnique = "herbicide";
-                    break;
-                case R.id.rdManualHerbicideControl:
-                    usesHerbicide = true;
-                    weedControlTechnique = "manual_herbicide";
-                    break;
+        rdgWeedControl!!.setOnCheckedChangeListener { radioGroup: RadioGroup?, radioIndex: Int ->
+            when (radioIndex) {
+                R.id.rdManualOnlyControl -> {
+                    usesHerbicide = false
+                    weedControlTechnique = "manual"
+                }
+
+                R.id.rdHerbicideControl -> {
+                    usesHerbicide = true
+                    weedControlTechnique = "herbicide"
+                }
+
+                R.id.rdManualHerbicideControl -> {
+                    usesHerbicide = true
+                    weedControlTechnique = "manual_herbicide"
+                }
             }
-        });
+        }
 
 
-        btnFinish.setOnClickListener(view -> validate(false));
-        btnCancel.setOnClickListener(view -> closeActivity(false));
-        showCustomNotificationDialog();
+        btnFinish!!.setOnClickListener { view: View? -> validate(false) }
+        btnCancel!!.setOnClickListener { view: View? -> closeActivity(false) }
+        showCustomNotificationDialog()
     }
 
-    @Override
-    protected void validate(boolean backPressed) {
+    override fun validate(backPressed: Boolean) {
         if (Strings.isEmptyOrWhitespace(weedControlTechnique)) {
-            showCustomWarningDialog(getString(R.string.lbl_invalid_selection), getString(R.string.lbl_weed_control_prompt));
-            return;
+            showCustomWarningDialog(
+                getString(R.string.lbl_invalid_selection),
+                getString(R.string.lbl_weed_control_prompt)
+            )
+            return
         }
 
         //get the user input
-        weedRadioIndex = rdgWeedControl.getCheckedRadioButtonId();
-        firstOperationCost = mathHelper.convertToDouble(editFirstWeedingOpCost.getText().toString());
-        secondOperationCost = mathHelper.convertToDouble(editSecondWeedingOpCost.getText().toString());
+        weedRadioIndex = rdgWeedControl!!.checkedRadioButtonId
+        firstOperationCost = mathHelper!!.convertToDouble(editFirstWeedingOpCost!!.text.toString())
+        secondOperationCost =
+            mathHelper!!.convertToDouble(editSecondWeedingOpCost!!.text.toString())
 
         if (firstOperationCost <= minCost) {
-            showCustomWarningDialog(getString(R.string.lbl_invalid_cost), getString(R.string.lbl_first_weeding_costs_prompt));
-            return;
+            showCustomWarningDialog(
+                getString(R.string.lbl_invalid_cost),
+                getString(R.string.lbl_first_weeding_costs_prompt)
+            )
+            return
         }
         if (secondOperationCost <= minCost) {
-            showCustomWarningDialog(getString(R.string.lbl_invalid_cost), getString(R.string.lbl_second_weeding_cost_prompt));
-            return;
+            showCustomWarningDialog(
+                getString(R.string.lbl_invalid_cost),
+                getString(R.string.lbl_second_weeding_cost_prompt)
+            )
+            return
         }
 
         try {
             if (currentPractice == null) {
-                currentPractice = new CurrentPractice();
+                currentPractice = CurrentPractice()
             }
             if (fieldOperationCost == null) {
-                fieldOperationCost = new FieldOperationCost();
+                fieldOperationCost = FieldOperationCost()
             }
 
-            currentPractice.setWeedControlTechnique(weedControlTechnique);
-            currentPractice.setUsesHerbicide(usesHerbicide);
-            currentPractice.setWeedRadioIndex(weedRadioIndex);
+            currentPractice!!.weedControlTechnique = weedControlTechnique
+            currentPractice!!.usesHerbicide = usesHerbicide
+            currentPractice!!.weedRadioIndex = weedRadioIndex
 
-            database.currentPracticeDao().insert(currentPractice);
+            database.currentPracticeDao().insert(currentPractice!!)
 
-            fieldOperationCost.setFirstWeedingOperationCost(firstOperationCost);
-            fieldOperationCost.setSecondWeedingOperationCost(secondOperationCost);
+            fieldOperationCost!!.firstWeedingOperationCost = firstOperationCost
+            fieldOperationCost!!.secondWeedingOperationCost = secondOperationCost
 
-            database.fieldOperationCostDao().insert(fieldOperationCost);
-            database.adviceStatusDao().insert(new AdviceStatus(EnumAdviceTasks.COST_OF_WEED_CONTROL.name(), true));
+            database.fieldOperationCostDao().insert(fieldOperationCost!!)
+            database.adviceStatusDao()
+                .insert(AdviceStatus(EnumAdviceTasks.COST_OF_WEED_CONTROL.name, true))
 
-            closeActivity(backPressed);
-        } catch (Exception ex) {
-            Toast.makeText(context, ex.getMessage(), Toast.LENGTH_SHORT).show();
-            Sentry.captureException(ex);
+            closeActivity(backPressed)
+        } catch (ex: Exception) {
+            Toast.makeText(context, ex.message, Toast.LENGTH_SHORT).show()
+            Sentry.captureException(ex)
         }
-
     }
 }
