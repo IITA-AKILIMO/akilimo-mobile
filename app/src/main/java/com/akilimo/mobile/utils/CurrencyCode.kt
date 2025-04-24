6f6312edf9353ec5c008e5359a8b392956b34de0
Masgeek <barsamms@gmail.com>
2025-04-24 14:33:36 +0300

    ♻️ refactor(utils): migrate `CurrencyCode` from Java to Kotlin
    - Rewrote `CurrencyCode` class in Kotlin for better interoperability and conciseness.
    - Changed the Java class to a Kotlin `object` to streamline singleton usage.
    
    This improves code readability, aligns with modern development practices, and enhances type safety.

--- app/src/main/java/com/akilimo/mobile/utils/CurrencyCode.kt
+++ app/src/main/java/com/akilimo/mobile/utils/CurrencyCode.kt
@@ -1,23 +1,17 @@
-package com.akilimo.mobile.utils;
+package com.akilimo.mobile.utils
 
-import android.util.Log;
+import com.mynameismidori.currencypicker.ExtendedCurrency
+import io.sentry.Sentry
 
+object CurrencyCode {
 
-import com.mynameismidori.currencypicker.ExtendedCurrency;
-
-import io.sentry.Sentry;
-
-public class CurrencyCode {
-
-    private static String LOG_TAG = CurrencyCode.class.getSimpleName();
-
-    public static ExtendedCurrency getCurrencySymbol(String currencyCode) {
-        ExtendedCurrency currency = ExtendedCurrency.getCurrencyByISO(currencyCode);
+    fun getCurrencySymbol(currencyCode: String?): ExtendedCurrency? {
+        val currency = ExtendedCurrency.getCurrencyByISO(currencyCode)
         try {
-            return currency;
-        } catch (Exception ex) {
-            Sentry.captureException(ex);
+            return currency
+        } catch (ex: Exception) {
+            Sentry.captureException(ex)
         }
-        return null;
+        return null
     }
 }
