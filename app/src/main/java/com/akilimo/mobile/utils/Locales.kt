ca94e561ab92b408516db52bc514ad7a8518638c
Masgeek <barsamms@gmail.com>
2025-04-24 14:49:52 +0300

    ♻️ refactor(locales): migrate `Locales` class from Java to Kotlin
    - Rewrote `Locales` class in Kotlin for better maintainability and modern language features.
    - Updated package structure to include `utils`.
    
    This change aligns with the goal of adopting Kotlin for improved code consistency and readability.

--- app/src/main/java/com/akilimo/mobile/utils/Locales.kt
+++ app/src/main/java/com/akilimo/mobile/utils/Locales.kt
@@ -1,18 +1,18 @@
-package com.akilimo.mobile;
+package com.akilimo.mobile.utils
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.Locale
 
-public class Locales {
-    private static final Locale LOCAL_TZ_SWA = new Locale("sw", "TZ");
-    private static final Locale LOCAL_KE_SWA = new Locale("sw", "KE");
-    private static final Locale LOCAL_NG_ENGLISH = new Locale("en", "NG");
-    private static final Locale LOCAL_RW_KINYARWANDA = new Locale("rw", "RW");
+object Locales {
+    private val LOCAL_TZ_SWA = Locale("sw", "TZ")
+    private val LOCAL_KE_SWA = Locale("sw", "KE")
+    private val LOCAL_NG_ENGLISH = Locale("en", "NG")
+    private val LOCAL_RW_KINYARWANDA = Locale("rw", "RW")
 
-    public static final List<Locale> APP_LOCALES = Arrays.asList(
-            Locale.ENGLISH,
-            Locales.LOCAL_TZ_SWA);
+    val APP_LOCALES: List<Locale> = listOf(
+        Locale.ENGLISH,
+        LOCAL_TZ_SWA
+    )
 
-    public static final List<Locale> LOCALE_COUNTRIES = Arrays.asList(Locales.LOCAL_TZ_SWA, LOCAL_NG_ENGLISH, Locales.LOCAL_RW_KINYARWANDA);
+    val LOCALE_COUNTRIES: List<Locale> =
+        listOf(LOCAL_TZ_SWA, LOCAL_NG_ENGLISH, LOCAL_RW_KINYARWANDA)
 }
