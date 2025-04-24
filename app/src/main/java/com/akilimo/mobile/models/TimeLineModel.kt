abb6a35c15b7a438363041ee50498a3bce519661
Masgeek <barsamms@gmail.com>
2025-04-24 14:33:22 +0300

    ✨ feat(models): update Parcelize import for TimeLineModel
    - Updated `Parcelize` import to use the latest `kotlinx.parcelize` package.
    
    Improves compatibility with newer Kotlin versions.

--- app/src/main/java/com/akilimo/mobile/models/TimeLineModel.kt
+++ app/src/main/java/com/akilimo/mobile/models/TimeLineModel.kt
@@ -2,7 +2,7 @@ package com.akilimo.mobile.models
 
 import android.os.Parcelable
 import com.akilimo.mobile.utils.enums.StepStatus
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class TimeLineModel(
