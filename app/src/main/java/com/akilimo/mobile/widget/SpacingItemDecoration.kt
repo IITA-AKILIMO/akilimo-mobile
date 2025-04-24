b885fc44c3fb90cfaaf5f31e7afe35bb4419e5a3
Masgeek <barsamms@gmail.com>
2025-04-24 14:40:19 +0300

    ✨ feat(widget): migrate `SpacingItemDecoration` to Kotlin
    - Converted `SpacingItemDecoration` class from Java to Kotlin.
    - Improved readability and leveraged Kotlin syntax for cleaner code.

--- app/src/main/java/com/akilimo/mobile/widget/SpacingItemDecoration.kt
+++ app/src/main/java/com/akilimo/mobile/widget/SpacingItemDecoration.kt
@@ -1,43 +1,41 @@
-package com.akilimo.mobile.widget;
+package com.akilimo.mobile.widget
 
-import android.graphics.Rect;
-import android.view.View;
-
-import androidx.recyclerview.widget.RecyclerView;
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 
 /**
  * RecyclerView item decoration - give equal margin around grid item
  */
-public class SpacingItemDecoration extends RecyclerView.ItemDecoration {
-
-    private int spanCount;
-    private int spacingPx;
-    private boolean includeEdge;
-
-    public SpacingItemDecoration(int spanCount, int spacingPx, boolean includeEdge) {
-        this.spanCount = spanCount;
-        this.spacingPx = spacingPx;
-        this.includeEdge = includeEdge;
-    }
-
-    @Override
-    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        int position = parent.getChildAdapterPosition(view); // item position
-        int column = position % spanCount; // item column
+class SpacingItemDecoration(
+    private val spanCount: Int,
+    private val spacingPx: Int,
+    private val includeEdge: Boolean
+) :
+    ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view) // item position
+        val column = position % spanCount // item column
 
         if (includeEdge) {
-            outRect.left = spacingPx - column * spacingPx / spanCount;
-            outRect.right = (column + 1) * spacingPx / spanCount;
+            outRect.left = spacingPx - column * spacingPx / spanCount
+            outRect.right = (column + 1) * spacingPx / spanCount
 
             if (position < spanCount) { // top edge
-                outRect.top = spacingPx;
+                outRect.top = spacingPx
             }
-            outRect.bottom = spacingPx; // item bottom
+            outRect.bottom = spacingPx // item bottom
         } else {
-            outRect.left = column * spacingPx / spanCount;
-            outRect.right = spacingPx - (column + 1) * spacingPx / spanCount;
+            outRect.left = column * spacingPx / spanCount
+            outRect.right = spacingPx - (column + 1) * spacingPx / spanCount
             if (position >= spanCount) {
-                outRect.top = spacingPx; // item top
+                outRect.top = spacingPx // item top
             }
         }
     }
