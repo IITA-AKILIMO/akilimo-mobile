b33b1606bfa86acb78a8cf3294f920b8852d92dc
Masgeek <barsamms@gmail.com>
2025-04-29 13:13:38 +0300

    refactor: move operation cost response to entities and update endpoint to use country code

--- app/src/main/java/com/akilimo/mobile/interfaces/AkilimoService.kt
+++ app/src/main/java/com/akilimo/mobile/interfaces/AkilimoService.kt
@@ -7,9 +7,9 @@ import com.akilimo.mobile.entities.FertilizerPriceResponse
 import com.akilimo.mobile.entities.FertilizerResponse
 import com.akilimo.mobile.entities.InvestmentAmountResponse
 import com.akilimo.mobile.entities.MaizePriceResponse
+import com.akilimo.mobile.entities.OperationCostResponse
 import com.akilimo.mobile.entities.PotatoPriceResponse
 import com.akilimo.mobile.entities.StarchFactoryResponse
-import com.akilimo.mobile.models.OperationCostResponse
 import com.akilimo.mobile.rest.request.RecommendationRequest
 import com.akilimo.mobile.rest.request.SurveyRequest
 import com.akilimo.mobile.rest.response.RecommendationResponse
@@ -49,9 +49,9 @@ interface AkilimoService {
     @POST("v1/recommendations")
     fun computeRecommendations(@Body recommendationRequest: RecommendationRequest?): Call<RecommendationResponse>
 
-    @GET("v1/operation-costs/currency/{currency_code}")
+    @GET("v1/operation-costs/country/{country_code}")
     fun getOperationCosts(
-        @Path("currency_code") currencyCode: String,
+        @Path("country_code") countryCode: String,
         @QueryMap queryParams: Map<String, String>
     ): Call<OperationCostResponse>
 
