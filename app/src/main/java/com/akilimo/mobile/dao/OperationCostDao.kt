abc855efa4b418d835ea9ba5384e96d06b26f6ae
Masgeek <barsamms@gmail.com>
2025-04-29 13:55:01 +0300

    ✨ feat(dao): add `OperationCostDao` interface for database operations
    - Implemented DAO for `OperationCost` entity with CRUD methods
    - Included queries to fetch, insert, update, and delete operation costs
    - Provided a method to delete all records from the `mandatory_info` table
    
    Enables efficient interaction with the database for `OperationCost` data management.

---
+++ app/src/main/java/com/akilimo/mobile/dao/OperationCostDao.kt
@@ -0,0 +1,34 @@
+package com.akilimo.mobile.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.akilimo.mobile.entities.OperationCost
+
+@Dao
+interface OperationCostDao {
+
+    @Query("SELECT * FROM operation_costs")
+    fun findAll(): List<OperationCost>
+
+    @Query("SELECT * FROM operation_costs LIMIT 1")
+    fun findOne(): OperationCost?
+
+    @Query("SELECT * FROM operation_costs where item_tag = :itemTag")
+    fun findOneByItemTag(itemTag: String): OperationCost?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(operationCost: List<OperationCost>)
+
+    @Update
+    fun update(operationCost: OperationCost)
+
+    @Delete
+    fun delete(operationCost: OperationCost?)
+
+    @Query("DELETE FROM mandatory_info")
+    fun deleteAll()
+}
