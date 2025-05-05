a5017593778aa824c2cbf9e6f040a9c6e0978a74
Masgeek <barsamms@gmail.com>
2025-05-05 15:35:12 +0300

    ✨ feat(UserInfo): add `@JsonProperty` annotations for field serialization
    - Added `@JsonProperty` annotations to enable proper JSON mapping for UserInfo fields
    - Removed `@JsonNaming` for enhanced control of individual field naming
    
    Improves JSON serialization consistency and aligns with API requirements.

--- app/src/main/java/com/akilimo/mobile/rest/request/UserInfo.kt
+++ app/src/main/java/com/akilimo/mobile/rest/request/UserInfo.kt
@@ -1,18 +1,36 @@
 package com.akilimo.mobile.rest.request
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
+import com.fasterxml.jackson.annotation.JsonProperty
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 class UserInfo {
+
+    @JsonProperty("device_token")
     var deviceToken: String = "NA"
+
+    @JsonProperty("phone_number")
     var phoneNumber: String = "NA"
+
+    @JsonProperty("user_name")
     var userName: String = "akilimo"
+
+    @JsonProperty("first_name")
     var firstName: String = "AKILIMO"
+
+    @JsonProperty("last_name")
     var lastName: String = "USER"
+
+    @JsonProperty("gender")
     var gender: String = "NA"
+
+    @JsonProperty("email")
     var email: String = "NA"
+
+    @JsonProperty("field_description")
     var fieldDescription: String = "NA"
+
+    @JsonProperty("send_sms")
     var sendSms: Boolean = false
+
+    @JsonProperty("send_email")
     var sendEmail: Boolean = false
 }
