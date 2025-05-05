0a819bcbda186a0d31730b9e693132c3832909d2
Masgeek <barsamms@gmail.com>
2025-05-05 11:43:14 +0300

    ✨ feat(utils, request): improve type safety & add JSON naming strategy
    - Updated `convertToUnitWeightPrice` to use `Double` for `unitWeight` for better precision.
    - Adjusted unit tests to reflect updated method signature.
    - Added `JsonNaming` annotation with `SnakeCaseStrategy` to `UserInfo` class.
    - Renamed fields in `UserInfo` to align with snake_case conversion.
    
    Improves data handling consistency and maintains JSON API standards.

--- app/src/main/java/com/akilimo/mobile/rest/request/UserInfo.kt
+++ app/src/main/java/com/akilimo/mobile/rest/request/UserInfo.kt
@@ -1,15 +1,17 @@
 package com.akilimo.mobile.rest.request
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 class UserInfo {
     var deviceToken: String = "NA"
-    var mobileCountryCode: String = "NA"
-    var mobileNumber: String = "NA"
-    var fullPhoneNumber: String = "NA"
-    var userName: String = "NA"
+    var phoneNumber: String = "NA"
+    var userName: String = "akilimo"
     var firstName: String = "AKILIMO"
     var lastName: String = "USER"
     var gender: String = "NA"
-    var emailAddress: String = "NA"
+    var email: String = "NA"
     var fieldDescription: String = "NA"
     var sendSms: Boolean = false
     var sendEmail: Boolean = false
