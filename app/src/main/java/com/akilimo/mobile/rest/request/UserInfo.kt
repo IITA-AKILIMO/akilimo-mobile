b6b14bedf95285290b22936c50c42eb4e5b9b1d5
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-05-06 14:57:17 +0300

    ♻️ refactor(UserInfo): convert `UserInfo` to a data class
    - Refactored `UserInfo` from a regular class to a data class.
    - Renamed JSON properties `email` to `email_address` and `field_description` to `farm_name`.
    
    This improves immutability and boilerplate reduction for the `UserInfo` model.

--- app/src/main/java/com/akilimo/mobile/rest/request/UserInfo.kt
+++ app/src/main/java/com/akilimo/mobile/rest/request/UserInfo.kt
@@ -2,35 +2,34 @@ package com.akilimo.mobile.rest.request
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-class UserInfo {
-
+data class UserInfo(
     @JsonProperty("device_token")
-    var deviceToken: String = "NA"
+    var deviceToken: String = "NA",
 
     @JsonProperty("phone_number")
-    var phoneNumber: String = "NA"
+    var phoneNumber: String = "NA",
 
     @JsonProperty("user_name")
-    var userName: String = "akilimo"
+    var userName: String = "akilimo",
 
     @JsonProperty("first_name")
-    var firstName: String = "AKILIMO"
+    var firstName: String = "AKILIMO",
 
     @JsonProperty("last_name")
-    var lastName: String = "USER"
+    var lastName: String = "USER",
 
     @JsonProperty("gender")
-    var gender: String = "NA"
+    var gender: String = "NA",
 
-    @JsonProperty("email")
-    var email: String = "NA"
+    @JsonProperty("email_address")
+    var emailAddress: String = "NA",
 
-    @JsonProperty("field_description")
-    var fieldDescription: String = "NA"
+    @JsonProperty("farm_name")
+    var farmName: String = "NA",
 
     @JsonProperty("send_sms")
-    var sendSms: Boolean = false
+    var sendSms: Boolean = false,
 
     @JsonProperty("send_email")
     var sendEmail: Boolean = false
-}
+)
