85ff144797f8e947ae2bb3e7cb138d928a1f89b7
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-05-07 12:03:26 +0300

    ✅ test(MathHelperTest): fix test case by updating unitWeight type from Int to Double
    - Updated `unitWeight` type to `Double` to match method signature requirements.
    - Ensured test case computes correctly without type mismatch.

--- app/src/test/java/com/akilimo/mobile/utils/MathHelperTest.kt
+++ app/src/test/java/com/akilimo/mobile/utils/MathHelperTest.kt
@@ -78,7 +78,7 @@ internal class MathHelperTest {
     @Test
     fun compute_price_by_per_tonne_of_cassava() {
         val expected = 166000.00
-        val unitWeight = 1000
+        val unitWeight = 1000.0
         val result = mathHelper.convertToUnitWeightPrice(166000.00, unitWeight)
         Assert.assertEquals(expected, result, 0.0)
     }
