10223a01ac96933497f41f619b51b40f1aad0c57
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-05-07 13:41:39 +0300

    ✨ feat(UserInfo): update default values for data class fields
    - Removed default value "NA" for `deviceToken`
    - Updated default value of `emailAddress` to "na@mail.com"
    
    Streamlines default settings to improve underlying data consistency.

--- app/src/main/java/com/akilimo/mobile/rest/request/UserInfo.kt
+++ app/src/main/java/com/akilimo/mobile/rest/request/UserInfo.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UserInfo(
     @JsonProperty("device_token")
-    var deviceToken: String = "NA",
+    var deviceToken: String,
 
     @JsonProperty("phone_number")
     var phoneNumber: String = "NA",
@@ -22,7 +22,7 @@ data class UserInfo(
     var gender: String = "NA",
 
     @JsonProperty("email_address")
-    var emailAddress: String? = null,
+    var emailAddress: String = "na@mail.com",
 
     @JsonProperty("farm_name")
     var farmName: String = "my_farm",
