11c05ca773aa45a617e1453866661a9ed3f65721
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-05-07 13:41:48 +0300

    ✨ feat(database): bump database version to 7
    - Updated database schema version from 6 to 7.
    - Ensured compatibility with new migration changes.

--- app/src/main/java/com/akilimo/mobile/dao/AppDatabase.kt
+++ app/src/main/java/com/akilimo/mobile/dao/AppDatabase.kt
@@ -52,7 +52,7 @@ import com.akilimo.mobile.entities.UserProfile
         UseCase::class,
         AkilimoCurrency::class,
         AdviceStatus::class
-    ], version = 6,
+    ], version = 7,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
