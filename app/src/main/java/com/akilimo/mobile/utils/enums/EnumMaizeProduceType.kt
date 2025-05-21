f6c47da8ca15d8632f93a46ca5b0af2c904dce0b
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-05-21 09:41:36 +0300

    ♻️ refactor(enums): simplify enum definitions for produce types
    - Removed abstract `produce()` methods from `EnumMaizeProduceType` and `EnumPotatoProduceType`
    - Converted enums to use simpler declarations without overriding methods
    
    This reduces complexity and improves maintainability of the code.

--- app/src/main/java/com/akilimo/mobile/utils/enums/EnumMaizeProduceType.kt
+++ app/src/main/java/com/akilimo/mobile/utils/enums/EnumMaizeProduceType.kt
@@ -1,21 +1,5 @@
 package com.akilimo.mobile.utils.enums
 
 enum class EnumMaizeProduceType {
-    GRAIN {
-        override fun produce(): String {
-            return "grain"
-        }
-    },
-    DRY_COB {
-        override fun produce(): String {
-            return "dry_cob"
-        }
-    },
-    FRESH_COB {
-        override fun produce(): String {
-            return "fresh_cob"
-        }
-    };
-
-    abstract fun produce(): String
+    GRAIN, DRY_COB, FRESH_COB;
 }
