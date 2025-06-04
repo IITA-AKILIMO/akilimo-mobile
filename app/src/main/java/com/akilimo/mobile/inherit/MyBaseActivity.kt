a4c3cd2dc2d672ec1f1f2e08647d6debf76b1df7
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-06-04 10:14:21 +0300

    Refactor: Remove deprecated `initToolbar` and `initComponent` methods from `MyBaseActivity`
    - Removed the overridden `initToolbar` method, which was marked as deprecated and advised to use `setupToolbar(toolbar, titleResId)` instead.
    - Removed the overridden `initComponent` method, which was also marked as deprecated.
    - Modified the `validate` method to log an error message to Sentry and throw an `UnsupportedOperationException` with a more specific message.

--- app/src/main/java/com/akilimo/mobile/inherit/MyBaseActivity.kt
+++ app/src/main/java/com/akilimo/mobile/inherit/MyBaseActivity.kt
@@ -1,21 +1,11 @@
 package com.akilimo.mobile.inherit
 
+import io.sentry.Sentry
+
 abstract class MyBaseActivity : BaseActivity() {
-    @Deprecated(
-        "Remove completely and use setupToolbar(toolbar, titleResId) instead.",
-        replaceWith = ReplaceWith("setupToolbar(binding.toolbarLayout.toolbar, R.string.your_title)"),
-        level = DeprecationLevel.WARNING
-    )
-    override fun initToolbar() {
-        throw UnsupportedOperationException()
-    }
-
-    @Deprecated("Deprecated remove it completely")
-    override fun initComponent() {
-        throw UnsupportedOperationException()
-    }
-
     override fun validate(backPressed: Boolean) {
-        throw UnsupportedOperationException()
+        val errorMsg = "validate Not implemented for this class"
+        Sentry.captureMessage(errorMsg)
+        throw UnsupportedOperationException(errorMsg)
     }
 }
