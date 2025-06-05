28dfef68a420767e5f5da0de7786aab4e5497700
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-06-05 11:55:43 +0300

    ♻️ refactor(BaseRecommendationActivity): simplify binding and adapter initialization
    - Refactored binding logic by extending `BindBaseActivity`
    - Simplified `RecOptionsAdapter` initialization and removed redundant properties
    - Eliminated manual binding lifecycle management
    
    [Improves code maintainability and reduces potential binding-related issues.]

--- app/src/main/java/com/akilimo/mobile/inherit/BaseRecommendationActivity.kt
+++ app/src/main/java/com/akilimo/mobile/inherit/BaseRecommendationActivity.kt
@@ -3,49 +3,36 @@ package com.akilimo.mobile.inherit
 import android.os.Bundle
 import androidx.viewbinding.ViewBinding
 import com.akilimo.mobile.adapters.RecOptionsAdapter
-import com.akilimo.mobile.dao.AppDatabase.Companion.getDatabase
 import com.akilimo.mobile.entities.AdviceStatus
 import com.akilimo.mobile.models.RecommendationOptions
 import com.akilimo.mobile.utils.enums.EnumAdviceTasks
 
-abstract class BaseRecommendationActivity<T : ViewBinding> : BaseActivity() {
-
-    private var _binding: T? = null
-    protected val binding
-        get() = _binding ?: throw IllegalStateException("Binding is not initialized yet.")
+abstract class BaseRecommendationActivity<T : ViewBinding> : BindBaseActivity<T>() {
 
     private val recList get() = getRecommendationOptions()
 
-    protected var mAdapter: RecOptionsAdapter = RecOptionsAdapter(emptyList())
-    protected var dataPositionChanged = 0
+    protected lateinit var mAdapter: RecOptionsAdapter
+    protected var dataPositionChanged: Int = -1
 
-    protected abstract fun inflateBinding(): T
+
     protected abstract fun getRecommendationOptions(): List<RecommendationOptions>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = inflateBinding()
-        setContentView(binding.root)
+        mAdapter = RecOptionsAdapter(applicationContext, recList)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
 
     override fun onResume() {
         super.onResume()
-        mAdapter.setData(recList, dataPositionChanged)
+        mAdapter.setData(recList)
     }
 
     protected fun checkStatus(taskName: EnumAdviceTasks): AdviceStatus {
-        val database = getDatabase(this)
         val adviceStatus = database.adviceStatusDao().findOne(taskName.name)
-
         if (adviceStatus != null) {
             return adviceStatus
         }
-
         return AdviceStatus(taskName.name, false)
     }
 }
