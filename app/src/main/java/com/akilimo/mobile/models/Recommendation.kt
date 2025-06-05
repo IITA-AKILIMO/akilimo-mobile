f8e3dd241a91771c5071ebf29bac26db9cb12fb0
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-06-05 13:46:30 +0300

    ⚠️ deprecate(models): mark Recommendation as deprecated
    - Added @Deprecated annotation to Recommendation.
    - Suggested usage of RecommendationOptions as replacement.

--- app/src/main/java/com/akilimo/mobile/models/Recommendation.kt
+++ app/src/main/java/com/akilimo/mobile/models/Recommendation.kt
@@ -3,6 +3,7 @@ package com.akilimo.mobile.models
 import android.graphics.drawable.Drawable
 import com.akilimo.mobile.utils.enums.EnumAdvice
 
+@Deprecated("Use RecommendationOptions instead", replaceWith = ReplaceWith("RecommendationOptions"))
 data class Recommendation(
     val recommendationName: String,
     val recCode: EnumAdvice,
