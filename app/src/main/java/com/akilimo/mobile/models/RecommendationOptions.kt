36d4932aff342d2a1faec6f1f8b4d5c8d34d9e54
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-06-05 13:46:42 +0300

    ♻️ refactor(models): migrate RecommendationOptions to data class
    - Replaced class with a data class for RecommendationOptions
    - Introduced default values for properties and added `recommendationCode`
    
    This change improves immutability, readability, and default initialization.

--- app/src/main/java/com/akilimo/mobile/models/RecommendationOptions.kt
+++ app/src/main/java/com/akilimo/mobile/models/RecommendationOptions.kt
@@ -1,15 +1,12 @@
 package com.akilimo.mobile.models
 
 import com.akilimo.mobile.entities.AdviceStatus
+import com.akilimo.mobile.utils.enums.EnumAdvice
 import com.akilimo.mobile.utils.enums.EnumAdviceTasks
 
-class RecommendationOptions(
-    recName: String,
-    adviceName: EnumAdviceTasks,
-    adviceStatus: AdviceStatus
-) {
-    var recName: String? = recName
-    var adviceName: EnumAdviceTasks? = adviceName
-    var statusImage: Int = 0
-    var adviceStatus: AdviceStatus? = adviceStatus
-}
+data class RecommendationOptions(
+    val recommendationName: String,
+    val adviceName: EnumAdviceTasks = EnumAdviceTasks.NOT_SELECTED,
+    val adviceStatus: AdviceStatus = AdviceStatus("", false),
+    val recommendationCode: EnumAdvice = EnumAdvice.NOT_SELECTED,
+)
