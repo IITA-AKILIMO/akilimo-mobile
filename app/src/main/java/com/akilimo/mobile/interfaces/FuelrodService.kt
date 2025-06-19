bb4b66833df07b2ebb038130f610327789456935
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-06-19 13:03:08 +0300

    ✨ feat(fuelrod): initialize FuelrodApi class with RetrofitManager dependency
    - Refactored FuelrodApi to instantiate FuelrodService lazily using RetrofitManager
    - Updated method to use the injected service for API calls
    
    This change improves dependency management and ensures proper initialization of Retrofit service in FuelrodApi class.

--- app/src/main/java/com/akilimo/mobile/interfaces/FuelrodService.kt
+++ app/src/main/java/com/akilimo/mobile/interfaces/FuelrodService.kt
@@ -5,12 +5,17 @@ import com.akilimo.mobile.rest.retrofit.RetrofitManager
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+class FuelrodApi(private val retrofitManager: RetrofitManager) {
 
-object FuelrodApi {
-    val apiService: FuelrodService by lazy {
-        RetrofitManager.fuelrod().create(FuelrodService::class.java)
+    private val service: FuelrodService by lazy {
+        retrofitManager.fuelrod().create(FuelrodService::class.java)
+    }
+
+    suspend fun readConfig(app: String): List<RemoteConfigResponse> {
+        return service.readConfig(app)
     }
 }
+
 
 interface FuelrodService {
     @GET("v1/remote-config/app-name/{app}")
