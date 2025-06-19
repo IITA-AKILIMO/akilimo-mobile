fb428d102da2d0f71917fa2818320346c72c1d71
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-06-19 13:02:43 +0300

    ✨ feat(app): initialize RetrofitManager with saved endpoints
    - Added PreferenceManager to retrieve saved endpoints
    - Set up RetrofitManager with retrieved endpoints during app startup
    
    This update ensures that RetrofitManager is properly initialized with user-configured endpoints stored in preferences, enabling dynamic server configuration at app launch.

--- app/src/main/java/com/akilimo/mobile/Akilimo.kt
+++ app/src/main/java/com/akilimo/mobile/Akilimo.kt
@@ -2,6 +2,8 @@ package com.akilimo.mobile
 
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
+import com.akilimo.mobile.rest.retrofit.RetrofitManager
+import com.akilimo.mobile.utils.PreferenceManager
 import com.blongho.country_data.World
 import com.jakewharton.threetenabp.AndroidThreeTen
 
@@ -11,6 +13,13 @@ class Akilimo : MultiDexApplication() {
         super.onCreate()
         // TODO: Revise and remove in future releases
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
+        val sessionManager = PreferenceManager(this@Akilimo)
+        val akilimoEndpoint = sessionManager.akilimoEndpoint
+        val fuelrodEndpoint = sessionManager.fuelrodEndpoint
+
+        RetrofitManager.init(this@Akilimo, akilimoEndpoint, fuelrodEndpoint)
 
         //@FIX This is used to enable proper vector support for android 4.4 and below, it causes crashing when firing up the application
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
