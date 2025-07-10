af72aa6e89fc1c04ee8d26e2b538f581b6149275
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-07-10 15:06:07 +0300

    ♻️ refactor(activity): migrate `CostBaseActivity` to utilize `OperationCostViewModel`
    - Replaced manual operation cost handling logic with `OperationCostViewModel`.
    - Integrated `viewModels` to observe `operationCosts` and trigger UI updates.
    - Simplified network and database interaction by delegating to ViewModel.
    
    Improves code clarity, promotes MVVM architecture adherence, and streamlines operation cost handling logic.

--- app/src/main/java/com/akilimo/mobile/inherit/CostBaseActivity.kt
+++ app/src/main/java/com/akilimo/mobile/inherit/CostBaseActivity.kt
@@ -1,69 +1,40 @@
 package com.akilimo.mobile.inherit
 
-import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.viewbinding.ViewBinding
-import com.akilimo.mobile.entities.OperationCostResponse
-import com.akilimo.mobile.interfaces.AkilimoApi
 import com.akilimo.mobile.utils.enums.EnumOperation
 import com.akilimo.mobile.utils.enums.EnumOperationMethod
-import io.sentry.Sentry
+import com.akilimo.mobile.viewmodels.OperationCostViewModel
+import com.akilimo.mobile.viewmodels.factory.OperationCostViewModelFactory
 
 
 abstract class CostBaseActivity<T : ViewBinding> : BindBaseActivity<T>() {
+
+    private val viewModel: OperationCostViewModel by viewModels {
+        OperationCostViewModelFactory(this.application)
+    }
+
+
     protected fun loadOperationCost(
         operationName: EnumOperation,
         operationType: EnumOperationMethod,
         dialogTitle: String,
         hintText: String
     ) {
-        val queryParams = mapOf(
-            "operation_type" to operationType.name,
-            "operation_name" to operationName.name,
-        )
-        val operationCosts = database.operationCostDao().findAllFiltered(
-            operationName.name,
-            operationType.name,
-            countryCode
-        )
-
-        if (operationCosts.isNotEmpty()) {
-            showDialogFullscreen(
-                operationName,
-                operationType,
-                countryCode,
-                dialogTitle,
-                hintText
-            )
-            return
+        viewModel.operationCosts.observe(this) { costList ->
+            if (costList.isNotEmpty()) {
+                showDialogFullscreen(
+                    operationName, operationType, countryCode, dialogTitle, hintText
+                )
+            }
         }
-        val call = AkilimoApi.apiService.getOperationCosts(countryCode, queryParams)
-        call.enqueue(object : retrofit2.Callback<OperationCostResponse> {
-            override fun onResponse(
-                call: retrofit2.Call<OperationCostResponse>,
-                response: retrofit2.Response<OperationCostResponse>
-            ) {
-                if (response.isSuccessful) {
-                    val list = response.body()!!.data
-                    database.operationCostDao().insertAll(list)
-                    showDialogFullscreen(
-                        operationName,
-                        operationType,
-                        countryCode,
-                        dialogTitle,
-                        hintText
-                    )
-                }
-            }
 
-            override fun onFailure(call: retrofit2.Call<OperationCostResponse>, t: Throwable) {
-                Toast.makeText(this@CostBaseActivity, t.message, Toast.LENGTH_LONG)
-                    .show()
-                Sentry.captureException(t)
-            }
-
-        })
+        viewModel.loadOperationCosts(
+            countryCode = countryCode,
+            operationName = operationName,
+            operationType = operationType
+        )
     }
-
 
     protected abstract fun showDialogFullscreen(
         operationName: EnumOperation,
