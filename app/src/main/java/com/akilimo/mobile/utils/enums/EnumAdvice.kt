8f1223a0825cf7a0a3d69c286a82ce97a212b11e
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-07-10 09:06:23 +0300

    ♻️ refactor(enums): deprecate EnumAdvice
    - Marked `EnumAdvice` as `@Deprecated("Remove this")` to signal future removal.
    - Adjusted code formatting for consistency.
    
    Prepares for the eventual cleanup of unused entries in the codebase.

--- app/src/main/java/com/akilimo/mobile/utils/enums/EnumAdvice.kt
+++ app/src/main/java/com/akilimo/mobile/utils/enums/EnumAdvice.kt
@@ -1,5 +1,6 @@
 package com.akilimo.mobile.utils.enums
 
+@Deprecated("Remove this")
 enum class EnumAdvice {
-    FR, BPP, IC_MAIZE, IC_SWEET_POTATO, SPH, WM,NOT_SELECTED
+    FR, BPP, IC_MAIZE, IC_SWEET_POTATO, SPH, WM, NOT_SELECTED
 }
