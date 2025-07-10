9d382e8089e725911e0fcd7e87d425ce782db105
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-07-10 12:03:52 +0300

    ♻️ refactor(activity): replace `UseCaseWithTasks` with `UseCaseTask` in ScheduledPlantingActivity and PlantingPracticesActivity
    - Updated task handling to use `UseCaseTask` instead of `UseCaseWithTasks`.
    - Refactored task loading logic to use `loadUseCaseTasks` method.
    - Simplified RecyclerView setup by introducing `setupRecyclerView` for initialization.
    - Streamlined navigation by updating logic to handle `UseCaseTask`.
    - Replaced `processRecommendations` with `launchRecommendationActivity` for improved clarity.
    
    Aligns with recent changes in task entity conventions and improves code consistency.

--- app/src/main/java/com/akilimo/mobile/views/activities/usecases/PlantingPracticesActivity.kt
+++ app/src/main/java/com/akilimo/mobile/views/activities/usecases/PlantingPracticesActivity.kt
@@ -2,15 +2,11 @@ package com.akilimo.mobile.views.activities.usecases
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.akilimo.mobile.R
-import com.akilimo.mobile.adapters.UseCaseTaskAdapter
 import com.akilimo.mobile.databinding.ActivityPlantingPracticesBinding
-import com.akilimo.mobile.entities.UseCase
+import com.akilimo.mobile.entities.UseCaseTask
 import com.akilimo.mobile.inherit.BaseUseCaseTaskActivity
-import com.akilimo.mobile.models.UseCaseWithTasks
 import com.akilimo.mobile.utils.enums.EnumTask
 import com.akilimo.mobile.utils.enums.EnumUseCase
 import com.akilimo.mobile.views.activities.CassavaMarketActivity
@@ -34,123 +30,71 @@ class PlantingPracticesActivity : BaseUseCaseTaskActivity<ActivityPlantingPracti
         setupToolbar(binding.toolbarLayout.toolbar, R.string.lbl_best_planting_practices) {
             closeActivity(false)
         }
-        binding.recyclerView.run {
-            layoutManager = LinearLayoutManager(this@PlantingPracticesActivity)
-            setHasFixedSize(true)
-            adapter = mAdapter
-        }
 
-        var useCase = database.useCaseDao().findOne()
-        if (useCase == null) {
-            useCase = UseCase()
-        }
+        setupRecyclerView(binding.recyclerView)
+
+
         binding.singleButton.btnAction.setOnClickListener {
             try {
-                useCase.apply {
-                    fertilizerRecommendation = false
-                    maizeInterCropping = false
-                    sweetPotatoInterCropping = false
-                    scheduledPlantingHighStarch = false
-                    scheduledPlanting = false
-                    bestPlantingPractices = true
-                    useCaseName = EnumUseCase.PP.name
-                }
-                database.useCaseDao().insertAll(useCase)
-                processRecommendations(this@PlantingPracticesActivity)
+                viewModel.insertUseCaseWithTasks(
+                    useCase = EnumUseCase.PP, useCaseTasks = loadUseCaseTasks()
+                )
+                launchRecommendationActivity(this@PlantingPracticesActivity)
             } catch (ex: Exception) {
                 Toast.makeText(this@PlantingPracticesActivity, ex.message, Toast.LENGTH_SHORT)
                     .show()
                 Sentry.captureException(ex)
             }
         }
+    }
 
+    override fun handleNavigation(useCaseTask: UseCaseTask) {
+        var intent: Intent? = null
+        when (useCaseTask.taskName) {
+            EnumTask.PLANTING_AND_HARVEST -> intent =
+                Intent(this@PlantingPracticesActivity, DatesActivity::class.java)
 
-        mAdapter.setOnItemClickListener(object : UseCaseTaskAdapter.OnItemClickListener {
-            override fun onItemClick(
-                view: View?,
-                recommendation: UseCaseWithTasks,
-                position: Int
-            ) {
-                var intent: Intent? = null
-                val advice = recommendation.adviceName
-                dataPositionChanged = position
-                when (advice) {
-                    EnumTask.PLANTING_AND_HARVEST -> intent =
-                        Intent(this@PlantingPracticesActivity, DatesActivity::class.java)
+            EnumTask.MARKET_OUTLET_CASSAVA -> intent =
+                Intent(this@PlantingPracticesActivity, CassavaMarketActivity::class.java)
 
-                    EnumTask.MARKET_OUTLET_CASSAVA -> intent =
-                        Intent(this@PlantingPracticesActivity, CassavaMarketActivity::class.java)
+            EnumTask.CURRENT_CASSAVA_YIELD -> intent =
+                Intent(this@PlantingPracticesActivity, RootYieldActivity::class.java)
 
-                    EnumTask.CURRENT_CASSAVA_YIELD -> intent =
-                        Intent(this@PlantingPracticesActivity, RootYieldActivity::class.java)
+            EnumTask.MANUAL_TILLAGE_COST -> intent = Intent(
+                this@PlantingPracticesActivity, ManualTillageCostActivity::class.java
+            )
 
-                    EnumTask.MANUAL_TILLAGE_COST -> intent = Intent(
-                        this@PlantingPracticesActivity, ManualTillageCostActivity::class.java
-                    )
+            EnumTask.TRACTOR_ACCESS -> intent =
+                Intent(this@PlantingPracticesActivity, TractorAccessActivity::class.java)
 
-                    EnumTask.TRACTOR_ACCESS -> intent =
-                        Intent(this@PlantingPracticesActivity, TractorAccessActivity::class.java)
+            EnumTask.COST_OF_WEED_CONTROL -> intent = Intent(
+                this@PlantingPracticesActivity, WeedControlCostsActivity::class.java
+            )
 
-                    EnumTask.COST_OF_WEED_CONTROL -> intent = Intent(
-                        this@PlantingPracticesActivity, WeedControlCostsActivity::class.java
-                    )
-
-                    else -> EnumTask.NOT_SELECTED
-                }
-                openActivity(intent)
-
-            }
-        })
+            else -> EnumTask.NOT_SELECTED
+        }
+        openActivity(intent)
     }
 
 
-    override fun getRecommendationOptions(): List<UseCaseWithTasks> {
-        val recommendations = getString(R.string.lbl_best_planting_practices)
-        val plantingString = getString(R.string.lbl_planting_harvest)
-        val marketOutletString = getString(R.string.lbl_market_outlet)
-        val weedControlCostString = getString(R.string.lbl_cost_of_weed_control)
-        val rootYieldString = getString(R.string.lbl_typical_yield)
-        val tillageOperationsString = getString(R.string.lbl_tillage_operations)
-        val manualTillageCostsString = getString(R.string.lbl_cost_of_manual_tillage)
-        val tractorAccessString = getString(R.string.lbl_tractor_access)
-
-        val myItems: MutableList<UseCaseWithTasks> = ArrayList()
-        myItems.add(
-            UseCaseWithTasks(
-                manualTillageCostsString,
-                EnumTask.MANUAL_TILLAGE_COST,
-                checkStatus(EnumTask.MANUAL_TILLAGE_COST)
-            )
+    override fun loadUseCaseTasks(): List<UseCaseTask> = listOf(
+        UseCaseTask(
+            taskLabel = R.string.lbl_cost_of_manual_tillage,
+            taskName = EnumTask.MANUAL_TILLAGE_COST
+        ),
+        UseCaseTask(taskLabel = R.string.lbl_tractor_access, taskName = EnumTask.TRACTOR_ACCESS),
+        UseCaseTask(
+            taskLabel = R.string.lbl_cost_of_weed_control,
+            taskName = EnumTask.COST_OF_WEED_CONTROL
+        ),
+        UseCaseTask(
+            taskLabel = R.string.lbl_typical_yield,
+            taskName = EnumTask.CURRENT_CASSAVA_YIELD
+        ),
+        UseCaseTask(
+            taskLabel = R.string.lbl_market_outlet,
+            taskName = EnumTask.MARKET_OUTLET_CASSAVA
         )
-        myItems.add(
-            UseCaseWithTasks(
-                tractorAccessString,
-                EnumTask.TRACTOR_ACCESS,
-                checkStatus(EnumTask.TRACTOR_ACCESS)
-            )
-        )
-        myItems.add(
-            UseCaseWithTasks(
-                weedControlCostString,
-                EnumTask.COST_OF_WEED_CONTROL,
-                checkStatus(EnumTask.COST_OF_WEED_CONTROL)
-            )
-        )
-        myItems.add(
-            UseCaseWithTasks(
-                rootYieldString,
-                EnumTask.CURRENT_CASSAVA_YIELD,
-                checkStatus(EnumTask.CURRENT_CASSAVA_YIELD)
-            )
-        )
-        myItems.add(
-            UseCaseWithTasks(
-                marketOutletString,
-                EnumTask.MARKET_OUTLET_CASSAVA,
-                checkStatus(EnumTask.MARKET_OUTLET_CASSAVA)
-            )
-        )
-        return myItems
-    }
+    )
 
 }
--- app/src/main/java/com/akilimo/mobile/views/activities/usecases/ScheduledPlantingActivity.kt
+++ app/src/main/java/com/akilimo/mobile/views/activities/usecases/ScheduledPlantingActivity.kt
@@ -2,16 +2,12 @@ package com.akilimo.mobile.views.activities.usecases
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.widget.Toolbar
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.akilimo.mobile.R
-import com.akilimo.mobile.adapters.UseCaseTaskAdapter
 import com.akilimo.mobile.databinding.ActivityScheduledPlantingBinding
-import com.akilimo.mobile.entities.UseCase
+import com.akilimo.mobile.entities.UseCaseTask
 import com.akilimo.mobile.inherit.BaseUseCaseTaskActivity
-import com.akilimo.mobile.models.UseCaseWithTasks
 import com.akilimo.mobile.utils.enums.EnumTask
 import com.akilimo.mobile.utils.enums.EnumUseCase
 import com.akilimo.mobile.views.activities.CassavaMarketActivity
@@ -32,86 +28,50 @@ class ScheduledPlantingActivity : BaseUseCaseTaskActivity<ActivityScheduledPlant
         setupToolbar(binding.toolbarLayout.toolbar, R.string.lbl_scheduled_planting_and_harvest) {
             closeActivity(false)
         }
-        binding.recyclerView.run {
-            layoutManager = LinearLayoutManager(this@ScheduledPlantingActivity)
-            setHasFixedSize(true)
-            adapter = mAdapter
-        }
+
+        setupRecyclerView(binding.recyclerView)
+
 
         binding.singleButton.btnAction.setOnClickListener {
-            var useCases = database.useCaseDao().findOne()
             try {
-                if (useCases == null) {
-                    useCases = UseCase()
-                }
-                useCases.apply {
-                    useCases.fertilizerRecommendation = false
-                    useCases.maizeInterCropping = false
-                    useCases.sweetPotatoInterCropping = false
-                    useCases.scheduledPlantingHighStarch = true
-                    useCases.scheduledPlanting = true
-                    useCases.bestPlantingPractices = false
-                    useCases.useCaseName = EnumUseCase.SP.name
-                }
-
-                database.useCaseDao().insertAll(useCases)
-                processRecommendations(this@ScheduledPlantingActivity)
+                viewModel.insertUseCaseWithTasks(
+                    useCase = EnumUseCase.SP, useCaseTasks = loadUseCaseTasks()
+                )
+                launchRecommendationActivity(this@ScheduledPlantingActivity)
             } catch (ex: Exception) {
                 Sentry.captureException(ex)
             }
         }
-
-        // on item list clicked
-        mAdapter.setOnItemClickListener(object : UseCaseTaskAdapter.OnItemClickListener {
-            override fun onItemClick(view: View?, recommendation: UseCaseWithTasks, position: Int) {
-                var intent: Intent? = null
-                dataPositionChanged = position
-                val advice = recommendation.adviceName
-                when (advice) {
-                    EnumTask.PLANTING_AND_HARVEST -> intent =
-                        Intent(this@ScheduledPlantingActivity, DatesActivity::class.java)
-
-                    EnumTask.MARKET_OUTLET_CASSAVA -> intent =
-                        Intent(this@ScheduledPlantingActivity, CassavaMarketActivity::class.java)
-
-                    EnumTask.CURRENT_CASSAVA_YIELD -> intent =
-                        Intent(this@ScheduledPlantingActivity, RootYieldActivity::class.java)
-
-                    else -> {}
-                }
-                openActivity(intent)
-            }
-        })
     }
 
-    override fun getRecommendationOptions(): List<UseCaseWithTasks> {
-        val plantingString = getString(R.string.lbl_planting_harvest)
-        val marketOutletString = getString(R.string.lbl_market_outlet)
-        val rootYieldString = getString(R.string.lbl_typical_yield)
+    override fun handleNavigation(useCaseTask: UseCaseTask) {
+        when (useCaseTask.taskName) {
+            EnumTask.PLANTING_AND_HARVEST -> intent =
+                Intent(this@ScheduledPlantingActivity, DatesActivity::class.java)
 
-        val myItems: MutableList<UseCaseWithTasks> =
-            ArrayList()
-        myItems.add(
-            UseCaseWithTasks(
-                plantingString,
-                EnumTask.PLANTING_AND_HARVEST,
-                checkStatus(EnumTask.PLANTING_AND_HARVEST)
-            )
-        )
-        myItems.add(
-            UseCaseWithTasks(
-                rootYieldString,
-                EnumTask.CURRENT_CASSAVA_YIELD,
-                checkStatus(EnumTask.CURRENT_CASSAVA_YIELD)
-            )
-        )
-        myItems.add(
-            UseCaseWithTasks(
-                marketOutletString,
-                EnumTask.MARKET_OUTLET_CASSAVA,
-                checkStatus(EnumTask.MARKET_OUTLET_CASSAVA)
-            )
-        )
-        return myItems
+            EnumTask.MARKET_OUTLET_CASSAVA -> intent =
+                Intent(this@ScheduledPlantingActivity, CassavaMarketActivity::class.java)
+
+            EnumTask.CURRENT_CASSAVA_YIELD -> intent =
+                Intent(this@ScheduledPlantingActivity, RootYieldActivity::class.java)
+
+            else -> {}
+        }
+        openActivity(intent)
     }
+
+    override fun loadUseCaseTasks(): List<UseCaseTask> = listOf(
+        UseCaseTask(
+            taskLabel = R.string.lbl_planting_harvest,
+            taskName = EnumTask.PLANTING_AND_HARVEST
+        ),
+        UseCaseTask(
+            taskLabel = R.string.lbl_typical_yield,
+            taskName = EnumTask.CURRENT_CASSAVA_YIELD
+        ),
+        UseCaseTask(
+            taskLabel = R.string.lbl_market_outlet,
+            taskName = EnumTask.MARKET_OUTLET_CASSAVA
+        )
+    )
 }
