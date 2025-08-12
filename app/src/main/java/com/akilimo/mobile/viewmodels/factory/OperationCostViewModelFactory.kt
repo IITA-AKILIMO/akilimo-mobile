abc4f8c0feebf97e58beb63423c21467bf8137df
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-08-12 14:16:33 +0300

    ♻️ refactor(factory): update `OperationCostViewModelFactory` to include `MathHelper`
    - Added `MathHelper` as a new dependency in `OperationCostViewModelFactory`.
    - Updated `createViewModel` method to pass `MathHelper` to `OperationCostViewModel`.
    
    Enhances dependency injection to support mathematical operations in `OperationCostViewModel`.

--- app/src/main/java/com/akilimo/mobile/viewmodels/factory/OperationCostViewModelFactory.kt
+++ app/src/main/java/com/akilimo/mobile/viewmodels/factory/OperationCostViewModelFactory.kt
@@ -1,13 +1,17 @@
 package com.akilimo.mobile.viewmodels.factory
 
 import android.app.Application
+import com.akilimo.mobile.utils.MathHelper
 import com.akilimo.mobile.viewmodels.OperationCostViewModel
 import com.akilimo.mobile.viewmodels.base.BaseViewModelFactory
 
-class OperationCostViewModelFactory(private val application: Application) :
+class OperationCostViewModelFactory(
+    private val application: Application,
+    private val mathHelper: MathHelper
+) :
     BaseViewModelFactory<OperationCostViewModel>() {
     override fun createViewModel(): OperationCostViewModel {
-        return OperationCostViewModel(application)
+        return OperationCostViewModel(application, mathHelper)
     }
 
 }
