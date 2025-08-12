fe6b81fdf7fcf7219d2c16dc94897b315d9b2e6a
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-08-12 08:55:19 +0300

    ♻️ refactor(factory): simplify `FertilizersViewModelFactory` with base class usage
    - Replaced `AndroidViewModelFactory` inheritance with `BaseViewModelFactory`.
    - Moved ViewModel creation logic to overridden `createViewModel` method.
    
    Enhances code reusability, simplifies factory logic, and aligns with consistent factory implementation.

--- app/src/main/java/com/akilimo/mobile/viewmodels/factory/FertilizersViewModelFactory.kt
+++ app/src/main/java/com/akilimo/mobile/viewmodels/factory/FertilizersViewModelFactory.kt
@@ -1,26 +1,20 @@
 package com.akilimo.mobile.viewmodels.factory
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.akilimo.mobile.exceptions.UnknownViewModelClassException
 import com.akilimo.mobile.viewmodels.FertilizersViewModel
+import com.akilimo.mobile.viewmodels.base.BaseViewModelFactory
 
 class FertilizersViewModelFactory(
     private val application: Application,
     private val minSelection: Int = 2,
     private val useCase: String?
-) : ViewModelProvider.AndroidViewModelFactory(application) {
-
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(FertilizersViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return FertilizersViewModel(
-                application = application,
-                minSelection = minSelection,
-                useCase = useCase
-            ) as T
-        }
-        throw UnknownViewModelClassException("Unknown ViewModel class")
+) : BaseViewModelFactory<FertilizersViewModel>() {
+    override fun createViewModel(): FertilizersViewModel {
+        return FertilizersViewModel(
+            application = application,
+            minSelection = minSelection,
+            useCase = useCase
+        )
     }
+
 }
