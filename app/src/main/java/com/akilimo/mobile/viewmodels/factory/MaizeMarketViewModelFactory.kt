f84121925257208c6b24c90908aefa24922b6680
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-08-12 11:49:04 +0300

    ♻️ refactor(factory): add `MathHelper` dependency to `MaizeMarketViewModelFactory`
    - Updated `MaizeMarketViewModelFactory` to include `MathHelper` as a parameter.
    - Modified `createViewModel` to pass `MathHelper` during ViewModel instantiation.
    
    Enhances dependency injection and supports improved modularity in ViewModel initialization.

--- app/src/main/java/com/akilimo/mobile/viewmodels/factory/MaizeMarketViewModelFactory.kt
+++ app/src/main/java/com/akilimo/mobile/viewmodels/factory/MaizeMarketViewModelFactory.kt
@@ -1,12 +1,16 @@
 package com.akilimo.mobile.viewmodels.factory
 
 import android.app.Application
+import com.akilimo.mobile.utils.MathHelper
 import com.akilimo.mobile.viewmodels.MaizeMarketViewModel
 import com.akilimo.mobile.viewmodels.base.BaseViewModelFactory
 
-class MaizeMarketViewModelFactory(private val application: Application) :
+class MaizeMarketViewModelFactory(
+    private val application: Application,
+    private val mathHelper: MathHelper
+) :
     BaseViewModelFactory<MaizeMarketViewModel>() {
     override fun createViewModel(): MaizeMarketViewModel {
-        return MaizeMarketViewModel(application)
+        return MaizeMarketViewModel(application, mathHelper)
     }
 }
