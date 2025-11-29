debebc8a3dee487edf57e5091cd1f40bc10ac4e5
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-11-29 13:40:51 +0300

    chore(enum): Add `produce` function to `EnumProduceType` for mapping produce types

--- app/src/main/java/com/akilimo/mobile/enums/EnumProduceType.kt
+++ app/src/main/java/com/akilimo/mobile/enums/EnumProduceType.kt
@@ -2,7 +2,15 @@ package com.akilimo.mobile.enums
 
 
 enum class EnumProduceType {
-    MAIZE_GRAIN, DRY_COB, MAIZE_FRESH_COB,
+    MAIZE_GRAIN, MAIZE_FRESH_COB,
 
-    SWEET_POTATO_TUBERS, SWEET_POTATO_FLOUR, UNKNOWN
+    SWEET_POTATO_TUBERS, SWEET_POTATO_FLOUR, UNKNOWN;
+
+    fun produce(): String = when (this) {
+        MAIZE_GRAIN -> "grain"
+        MAIZE_FRESH_COB -> "fresh_cob"
+        SWEET_POTATO_TUBERS -> "tubers"
+        SWEET_POTATO_FLOUR -> "flour"
+        UNKNOWN -> "Unknown"
+    }
 }
