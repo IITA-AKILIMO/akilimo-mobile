2bc22b6c9471de7342d975660d4f8a6f9297b24b
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-11-30 08:06:45 +0300

    chore(ui): Add `EnumUseCase` handling in various activities for improved use case specificity

--- app/src/main/java/com/akilimo/mobile/ui/activities/IcSweetPotatoActivity.kt
+++ app/src/main/java/com/akilimo/mobile/ui/activities/IcSweetPotatoActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import com.akilimo.mobile.base.AbstractRecommendationActivity
 import com.akilimo.mobile.dto.UseCaseOption
 import com.akilimo.mobile.enums.EnumAdviceTask
+import com.akilimo.mobile.enums.EnumUseCase
 import com.akilimo.mobile.ui.usecases.CassavaMarketActivity
 import com.akilimo.mobile.ui.usecases.DatesActivity
 import com.akilimo.mobile.ui.usecases.InterCropFertilizersActivity
 import com.akilimo.mobile.ui.usecases.SweetPotatoMarketActivity
 
-class IcSweetPotatoActivity : AbstractRecommendationActivity() {
+class IcSweetPotatoActivity : AbstractRecommendationActivity(
+    enumUseCase = EnumUseCase.CIS
+) {
     override fun getAdviceOptions() = listOf(
         UseCaseOption(EnumAdviceTask.AVAILABLE_FERTILIZERS_CIS),
         UseCaseOption(EnumAdviceTask.PLANTING_AND_HARVEST),
@@ -25,7 +28,10 @@ class IcSweetPotatoActivity : AbstractRecommendationActivity() {
 
         EnumAdviceTask.PLANTING_AND_HARVEST -> Intent(this, DatesActivity::class.java)
         EnumAdviceTask.CASSAVA_MARKET_OUTLET -> Intent(this, CassavaMarketActivity::class.java)
-        EnumAdviceTask.SWEET_POTATO_MARKET_OUTLET -> Intent(this, SweetPotatoMarketActivity::class.java)
+        EnumAdviceTask.SWEET_POTATO_MARKET_OUTLET -> Intent(
+            this,
+            SweetPotatoMarketActivity::class.java
+        )
 
         else -> null
     }
