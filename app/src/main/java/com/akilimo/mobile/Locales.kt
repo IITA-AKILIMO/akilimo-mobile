43c3f4b56c95ea34a54d163ef6bf47e6ddb2408e
masgeek <barsamms@gmail.com>
2026-02-24 11:06:28 +0300

    🌐 i18n(auth): add french language support
    Add French to the supported locales list and update variable naming to
    lowercase for consistency across the Locales object.

--- app/src/main/java/com/akilimo/mobile/Locales.kt
+++ app/src/main/java/com/akilimo/mobile/Locales.kt
@@ -5,13 +5,15 @@ import java.util.Locale
 
 object Locales {
 
-    val Swahili: Locale = Locale.forLanguageTag("sw-TZ")
-    val English: Locale = Locale.forLanguageTag("en-US")
-    val Kinyarwanda: Locale = Locale.forLanguageTag("rw-RW")
+    val swahili: Locale = Locale.forLanguageTag("sw-TZ")
+    val french: Locale = Locale.forLanguageTag("fr")
+    val english: Locale = Locale.forLanguageTag("en-US")
+    val kinyarwanda: Locale = Locale.forLanguageTag("rw-RW")
 
     val supportedLocales: List<Locale> = listOf(
-        English,
-        Swahili,
-        Kinyarwanda
+        english,
+        french,
+        swahili,
+        kinyarwanda
     )
 }
